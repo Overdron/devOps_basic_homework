e70fe57864f3b8588f3b42b457b926f5863ce729
root <root@vm2.ru-central1.internal>
2024-05-06 16:09:28 +0000

    hw7

--- init/vminit.sh
+++ init/vminit.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo -s
-apt update
 apt install mc -y
-cat ~/devOps_basic_homework/init/win_id_rsa.pub >> ~/.ssh/authrisedkeys
-apt install maven -y
-apt install default-jdk -y
+cat win_id_rsa.pub >> /home/user2/.ssh/authrisedkeys
+#apt install maven -y
+#apt install default-jdk -y
 apt install docker.io -y
+apt install docker-compose -y
+export CURRENT_DIR=&(pwd)
+cd /
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+cd $CURRENT_DIR
+
